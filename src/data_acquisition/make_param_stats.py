02b6eea14204728654d44a6542ade96e2f215a9d
Zach Keskinen <Zachkeskinen@gmail.com>
2023-09-01 10:40:21 -0600

    parameter figures and stats

--- src/data_acquisition/make_param_stats.py
+++ src/data_acquisition/make_param_stats.py
@@ -50,7 +50,7 @@ def make_stat_da(iter_num, loc_fp):
     elev = np.load(loc_fp.joinpath('elev.npy'))
     trees = np.load(loc_fp.joinpath('trees.npy'))
 
-    idx = (trees < 0.8) & (elev > 2000)
+    idx = (trees < 0.1) & (elev > 2000)
 
     if 'Cottonwood' in loc_fp.stem:
         print(f"Size of datarray: {res_ds['pearsonr'].data.shape}")
@@ -79,7 +79,7 @@ def make_stat_da(iter_num, loc_fp):
     res_ds.to_netcdf(loc_fp.parent.joinpath('stats_ncs', loc_fp.stem + '_stats.nc'))
 
 
-out_fp = Path('/bsuhome/zacharykeskinen/scratch/spicy/param_stats.nc')
+out_fp = Path('/bsuhome/zacharykeskinen/scratch/spicy/param_stats_notrees.nc')
 
 if out_fp.exists():
     print('Already exists...')
