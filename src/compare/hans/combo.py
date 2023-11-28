f241d83b8a0f90cd722ff7cb5336e883b5b249c3
Zach Keskinen <Zachkeskinen@gmail.com>
2023-11-28 16:35:48 -0700

    comaprsion done

--- src/compare/hans/combo.py
+++ src/compare/hans/combo.py
@@ -7,20 +7,31 @@ from pathlib import Path
 from tqdm import tqdm
 print('starting')
 
+in_dir = Path('~/scratch/spicy/SnowEx-Data/').expanduser().resolve()
+in_dir = Path('~/spicy-snow/SnowEx-Data-Hans/').expanduser().resolve()
+
+out_dir = Path('~/scratch/spicy/SnowEx-Hans-s1').expanduser()
+
+dss = {fp.stem: xr.open_dataset(fp) for fp in in_dir.glob('*.nc')}
+
 data_dir = Path('~/scratch/West_US_Canada').expanduser()
-assert data_dir.exists()
-fps = list(data_dir.glob('*.nc'))
+# assert data_dir.exists()
+fps = list(data_dir.glob('snd_*.nc'))
 
-b_ds = xr.open_dataset('~/scratch/spicy/SnowEx-Data/Banner_2021-03-15.nc')
+for stem, old_ds in dss.items():
+    new_dss = []
+    print(stem)
+    for fp in tqdm(fps):
+        dt = pd.to_datetime(fp.stem.split('_')[1])
+        if dt > old_ds.time[0] and dt < old_ds.time[-1]:
+            ds = xr.open_dataset(fp)['snd']
+            ds = ds.expand_dims(time = [dt])
+            ds = ds.sel(lat = slice(old_ds.y.max(), old_ds.y.min()), lon = slice(old_ds.x.min(), old_ds.x.max()))
+            new_dss.append(ds)
 
-dss = []
-for fp in tqdm(fps):
-    dt = pd.to_datetime(fp.stem.split('_')[1])
-    if dt > pd.to_datetime('2020-08-01') and dt < pd.to_datetime('2021-04-01'):
-        ds = xr.open_dataset(fp)['snd']
-        ds = ds.expand_dims(time = [dt])
-        ds = ds.sel(lat = slice(b_ds.y.max(), b_ds.y.min()), lon = slice(b_ds.x.min(), b_ds.x.max()))
-        dss.append(ds)
-full = xr.concat(dss, dim = 'time')
+    full = xr.concat(new_dss, dim = 'time')
+    full = full.dropna(dim ='time', how ='all').sortby('time')
+    full = full.rename({'lat':'y'})
+    full = full.rename({'lon':'x'})
 
-full.to_netcdf(data_dir.joinpath('combo.nc'))
+    full.to_netcdf(out_dir.joinpath(stem+'.nc'))
